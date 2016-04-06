4c623a0659dee21140103a6de91c0de32498af2f
Hom, Kenneth <khom@admins-macbook-air.local>
2016-04-06 16:27:18 -0400

    Initial Commit

---
+++ Exercism-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Exercism-iOS
+//
+//  Created by Hom, Kenneth on 4/6/16.
+//  Copyright © 2016 Hom, Kenneth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Exercism-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Exercism-iOS
+//
+//  Created by Hom, Kenneth on 4/6/16.
+//  Copyright © 2016 Hom, Kenneth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
